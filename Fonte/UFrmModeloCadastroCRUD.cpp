2005a70505a580e86ec5a65700e302daf909fe4e
Vlademiro <vlad@altersoft.net>
2021-03-12 02:57:13 -0300

    Aula 07 - Criacao do formulario modelo - https://www.youtube.com/watch?v=5fB3WRVgmcU&list=PLr5fNRPNxagxdDomz-V5oVy9NQuDIGSLr&index=7

---
+++ Fonte/UFrmModeloCadastroCRUD.cpp
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "UFrmModeloCadastroCRUD.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TFrmModeloCadastroCRUD *FrmModeloCadastroCRUD;
+//---------------------------------------------------------------------------
+__fastcall TFrmModeloCadastroCRUD::TFrmModeloCadastroCRUD(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
---
+++ Fonte/UFrmModeloCadastroCRUD.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef UFrmModeloCadastroCRUDH
+#define UFrmModeloCadastroCRUDH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TFrmModeloCadastroCRUD : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TFrmModeloCadastroCRUD(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFrmModeloCadastroCRUD *FrmModeloCadastroCRUD;
+//---------------------------------------------------------------------------
+#endif
